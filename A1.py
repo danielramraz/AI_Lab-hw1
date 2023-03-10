78f041e9978e4c829b51f8ec4af6b70fca5fe9c6
MichalMarom <100923568+MichalMarom@users.noreply.github.com>
2023-03-10 12:34:11 +0200

    Typo

--- A1.py
+++ A1.py
@@ -41,7 +41,7 @@ def genetic_algorithm(pop_size, num_genes, fitness_func, max_generations, cross_
         genTime = time.time()                                                       # information
         print("=========================================")
         print(f"the start time for this gen is {time.asctime(time.gmtime(genTime))}")
-        print(f"--------avarege for this gen is {avarege_fitness(fitnesses[0])}")
+        print(f"--------averege for this gen is {averege_fitness(fitnesses[0])}")
                 
         fitnesses_np = np.array(fitnesses)
         plt.hist(fitnesses_np)
@@ -90,11 +90,11 @@ def bulls_eye(individual, target, score):                   # explotation
             score += 5
     return score
 
-def avarege_fitness(fitness):                               # information
-    avarege = sum(fitness) / len(fitness)
-    variance = sum([((x-avarege)**2) for x in fitness]) / len(fitness)
+def averege_fitness(fitness):                               # information
+    averege = sum(fitness) / len(fitness)
+    variance = sum([((x-averege)**2) for x in fitness]) / len(fitness)-1
     sd = variance**0.5
-    return (avarege, sd)
+    return (averege, sd)
 
 
 
