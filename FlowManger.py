86b3f59eb7e13a180ea9fb96373ff25c986444ca
MichalMarom <100923568+MichalMarom@users.noreply.github.com>
2023-04-14 15:39:47 +0300

    UPDATE

--- FlowManger.py
+++ FlowManger.py
@@ -3,19 +3,38 @@ import Population
 import PopulationLab2
 # ----------- Python Package -----------
 import time
-from threading import Thread
+import threading
+# ----------- Consts Name  ----------
+NUM_ISLANDS = 2
 
 
 class FlowManger:
     population: PopulationLab2
 
     def __init__(self, current_time: time):
+        self.total_time = current_time
 
         # ----------- Program without threads -----------
-        self.total_time = current_time
-        self.population = PopulationLab2.PopulationLab2()
+        # self.population = PopulationLab2.PopulationLab2()
 
         # ----------- Program with the island model -----------
+        # Initialize the populations for each island
+        populations = []
+        for i in range(NUM_ISLANDS):
+            population = []  # Initialize the population for the current island
+            self.populations.append(PopulationLab2.PopulationLab2())
+            # Create and start threads for each island
+
+        threads = []
+        for i in range(NUM_ISLANDS):
+            thread = threading.Thread(target=populations[i].genetic_algorithm(), args=(i, populations[i]))
+            thread.start()
+            threads.append(thread)
+
+        # Wait for all threads to finish
+        for thread in threads:
+            thread.join()
+
         # self.threads = []
         # for thr in range(2):
         #     thread = Thread(target=PopulationLab2.PopulationLab2(),
