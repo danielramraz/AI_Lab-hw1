09557e0d30b512fd552748d36a370bf3c633e63b
danielramraz <79479383+danielramraz@users.noreply.github.com>
2023-04-21 20:11:49 +0300

    fix some comments

--- FlowManager.py
+++ FlowManager.py
@@ -2,7 +2,7 @@
 from Individual import Individual
 from Migration import Migration
 import Population
-import PopulationLab2
+import Population
 # ----------- Python Package -----------
 import time
 import threading
@@ -22,7 +22,7 @@ multi_tests_setting_vectors = [[3, 2, 0, 4, 7, 1, 2, 0, 0, 1],
 
 class FlowManager:
     
-    population: PopulationLab2
+    population: Population
     islands: list
     results: list 
     migration: Migration
@@ -36,10 +36,10 @@ class FlowManager:
 
     def run_single_population_solution(self):
         # ----------- single run by user -----------
-        # self.population = PopulationLab2.PopulationLab2()
+        # self.population = Population.Population()
 
         # ----------- test Program single thread -----------
-        self.population = PopulationLab2.PopulationLab2(single_test_setting_vector)
+        self.population = Population.Population(single_test_setting_vector)
         
         self.population.genetic_algorithm()
         self.results.append(self.population.best_individual)
@@ -49,7 +49,7 @@ class FlowManager:
         # Initialize the populations for each island
         for i in range(NUM_ISLANDS):
             # Initialize the population for the current island
-            self.islands.append(PopulationLab2.PopulationLab2(multi_tests_setting_vectors[i]))
+            self.islands.append(Population.Population(multi_tests_setting_vectors[i]))
         
         # Create and start threads for each island
         threads = []
@@ -77,15 +77,22 @@ class FlowManager:
         for index in range(len(self.results)):
             print(f"Best individual {index}:", self.results[index].gen)
             print(f"Best fitness {index}:", self.results[index].score)
-        
-        # bad_bins = 0
-        # self.population.best_individual.gen = list(filter(None, self.population.best_individual.gen))
-        # for item in self.population.best_individual.gen:
-        #     if sum(item) > self.population.max_weight:
-        #         bad_bins += 1
-        # print("BAD bins:", bad_bins)
-        # print("num bins:", len(self.population.best_individual.gen))
 
+    #     if single_test_setting_vector[0] == 2 or multi_tests_setting_vectors[0][0] == 2 :
+    #         self.extra_print_solution_bin_packing()
+    #     return
+        
+    # def extra_print_solution_bin_packing(self):
+    #     bad_bins = 0
+    #     self.population.best_individual.gen = list(filter(None, self.population.best_individual.gen))
+    #     for item in self.population.best_individual.gen:
+    #         if sum(item) > self.population.max_weight:
+    #             bad_bins += 1
+    #     print("BAD bins:", bad_bins)
+    #     print("num bins:", len(self.population.best_individual.gen))
+
+    #     return
+    
     def print_time(self):
         print(f"the total time for this algo is {time.time() - self.total_time} sec")
         print(f"the ticks time for this algo is {int(time.perf_counter())}")
